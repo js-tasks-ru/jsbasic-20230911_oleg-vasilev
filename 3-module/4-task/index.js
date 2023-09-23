38133a371772a44c33e4f786b11d7e81621ed2dc
Oleg,V <v.oleg2908@gmail.com>
2023-09-23 17:28:13 +0300

    Функция showSalary(users, age)

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,15 @@
-function showSalary(users, age) {
-  // ваш код...
+const showSalary = (users, age) => {
+  return users.filter((user) => (user.age <= age))
+                  .map((user) => `${user.name}, ${user.balance}`)
+                      .join('\n')
 }
+
+// const showSalary = (user, age) => {
+//   let arr = [];
+//   for (let item of user) {
+//       if(item.age <= age) {
+//           arr.push(`${item.name} ${item.balance}`);
+//       }
+//   }
+//   return arr.join(`\n ` );
+// }
